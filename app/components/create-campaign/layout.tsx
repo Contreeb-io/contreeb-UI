b2545df08f4a1e099cccda446d2f1e41f7895bb7
saeedyussif663 <saeedyussif663@gmail.com>
2025-11-04 12:25:01 +0000

    fixed image width issue

--- app/components/create-campaign/layout.tsx
+++ app/components/create-campaign/layout.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 
 export default function FormLayout({ children }: { children: ReactNode }) {
   return (
-    <section className="min-h-svh bg-white/40 bg-[url('/top.webp')] px-5 pb-10 backdrop-blur-[980px]">
+    <section className="min-h-svh bg-white/40 bg-[url('/top.webp')] bg-cover bg-center bg-no-repeat px-5 pb-10 backdrop-blur-[980px]">
       <div className="font-mackinac w-full max-w-[1440px]">
         <div className="flex h-16 w-full items-center justify-between">
           <p className="text-xl font-bold text-[#010040]">Contreebute.io</p>
