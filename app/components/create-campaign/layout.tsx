5378700c012900c6cd52d25a95bdeb7f53574447
saeedyussif663 <saeedyussif663@gmail.com>
2025-11-04 13:49:36 +0000

    added a pre-load

--- app/components/create-campaign/layout.tsx
+++ app/components/create-campaign/layout.tsx
@@ -7,7 +7,9 @@ export default function FormLayout({ children }: { children: ReactNode }) {
     <section className="min-h-svh bg-white/40 bg-[url('/top.webp')] bg-cover bg-center bg-no-repeat px-5 pb-10 backdrop-blur-[980px]">
       <div className="font-mackinac w-full max-w-[1440px]">
         <div className="flex h-16 w-full items-center justify-between">
-          <p className="text-xl font-bold text-[#010040]">Contreebute.io</p>
+          <Link to={"/"}>
+            <p className="text-xl font-bold text-[#010040]">Contreebute.io</p>
+          </Link>
 
           <Link to={"/"} className="size-9 rounded-full bg-[#E3EFFC] p-2">
             <X size={20} color="#101928" />
