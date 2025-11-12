1b1bd112b628b28f9bff221447bef3132abf8aae
saeedyussif663 <saeedyussif663@gmail.com>
2025-11-12 20:23:25 +0000

    color on mobile and image desktop

--- app/components/create-campaign/layout.tsx
+++ app/components/create-campaign/layout.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router";
 export default function FormLayout({ children }: { children: ReactNode }) {
   return (
     <section className="relative isolate min-h-svh px-5 pb-10">
-      <div className="absolute inset-0 -z-10 bg-[radial-gradient(circle,rgba(125,68,182,0.5)_0%,rgba(183,111,255,0.43)_100%)] blur-[490px]" />
+      <div className="absolute inset-0 -z-10 bg-[radial-gradient(circle,rgba(125,68,182,0.5)_0%,rgba(183,111,255,0.43)_100%)] blur-[70px] md:blur-[490px]" />
 
       <div className="font-mackinac w-full max-w-[1440px]">
         <div className="flex h-16 w-full items-center justify-between">
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -16,7 +16,7 @@ export const links: Route.LinksFunction = () => [
 
 export default function Home() {
   return (
-    <section className="flex h-svh items-center justify-center overflow-hidden bg-[url('/image-mobile.webp')] bg-cover bg-no-repeat px-4 sm:bg-[url('/bg-image.webp')]">
+    <section className="flex h-svh items-center justify-center overflow-hidden bg-[url('/image-mobile.webp')] bg-cover bg-no-repeat px-4 sm:bg-[url('/image-desktop.webp')]">
       <div className="absolute inset-0 top-[90px]">
         <img
           src={"/bg-cover.webp"}
