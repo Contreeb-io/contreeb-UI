227d5823e982d68831ba40f95a02f2e2ae1c745f
saeedyussif663 <saeedyussif663@gmail.com>
2025-11-27 10:59:11 +0000

    user guide finished

--- app/routes/user-guide.tsx
+++ app/routes/user-guide.tsx
@@ -1,4 +1,5 @@
 import { X } from "lucide-react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
 const faqSections = [
@@ -45,10 +46,28 @@ const faqSections = [
 ];
 
 export default function UserGuide() {
+  const [changeBg, setChangeBg] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 40) {
+        setChangeBg(true);
+      } else {
+        setChangeBg(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {};
+  }, []);
+
   return (
     <main className="relative isolate min-h-svh pb-10">
       <div className="absolute inset-0 -z-10 bg-[radial-gradient(circle,rgba(125,68,182,0.5)_0%,rgba(183,111,255,0.43)_100%)] blur-[70px] md:blur-[490px]" />
-      <div className="fixed top-0 z-10 w-full bg-[#FFFFFF29]">
+      <div
+        className={`fixed top-0 z-10 w-full ${!changeBg ? "bg-[#FFFFFF29]" : "bg-white/50"} `}
+      >
         <header className="mx-auto flex max-w-[1500px] items-center justify-between px-6 py-3">
           <div className="flex items-center gap-2">
             <h1 className="text-xl font-bold text-[#010040]">Contreebute.io</h1>
@@ -258,6 +277,252 @@ export default function UserGuide() {
             <h1 className="font-mackinac font-medium text-[#0E021A]">
               4. How Donors Give (So Simple, So Kind)
             </h1>
+            <div className="space-y-1.5 text-sm text-[#5D5757]">
+              <ul className="list-inside list-decimal space-y-1 pl-1.5 md:pl-4">
+                <li>Click your campaign link</li>
+                <li>See your story + items</li>
+                <li>
+                  Choose:
+                  <ul className="list-inside list-disc">
+                    <li className="pl-1.5 md:pl-4">
+                      Donate to a specific item (“I’ll fund the laptop!”)
+                    </li>
+                    <li className="pl-1.5 md:pl-4">Or give generally</li>
+                  </ul>
+                </li>
+                <li>Input your email for payment receipt.</li>
+                <li>
+                  Pay safely with{" "}
+                  <span className="font-bold">Mobile Money</span> ,{" "}
+                  <span className="font-bold">Card</span> ,{" "}
+                  <span className="font-bold">Bank</span>,{" "}
+                  <span className="font-bold">Apple Pay</span> (via Paystack)
+                </li>
+              </ul>
+              <p>Every cedi is tracked. Every donor is thanked.</p>
+            </div>
+          </article>
+
+          <article className="space-y-4" id="fees">
+            <h1 className="font-mackinac font-medium text-[#0E021A]">
+              5. Fees: Only If You Win (And You Keep 94%-96% of It)
+            </h1>
+            <div className="space-y-1.5 text-sm text-[#5D5757]">
+              <table>table</table>
+              <p>No money raised? You pay GHS 0. Always!</p>
+            </div>
+          </article>
+
+          <article className="space-y-4" id="when-how-paid">
+            <h1 className="font-mackinac font-medium text-[#0E021A]">
+              6. When & How You Get Paid
+            </h1>
+            <div className="space-y-1.5 text-sm text-[#5D5757]">
+              <p>Here’s exactly what happens the moment your campaign ends:</p>
+              <ul className="list-inside list-decimal space-y-1.5">
+                <li>
+                  <span className="font-bold">
+                    We quickly check everything (Public Campaigns Only)
+                  </span>
+                  <p className="pl-1.5 md:pl-4">
+                    For Public campaigns, our team does one final 2-minute
+                    review of your documents (just to protect everyone). This
+                    usually takes less than 10 minutes to complete.
+                  </p>
+                </li>
+                <li>
+                  <span className="font-bold">
+                    Fees are deducted automatically
+                  </span>
+                  <p className="pl-1.5 md:pl-4">
+                    Only the small fee we already showed you. No hidden charges!
+                  </p>
+                </li>
+                <li>
+                  <span className="font-bold">
+                    Your money reaches you in 1–3 business days
+                  </span>
+                  <p className="pl-1.5 md:pl-4">
+                    Whether you chose Mobile Money or bank account, it arrives
+                    safely within 1–3 business days. You’ll see the notification
+                    from Contreebute Technologies (that’s our official
+                    registered name).
+                  </p>
+                </li>
+                <li>
+                  <span className="font-bold">
+                    You get a clean, clear and loving receipt instantly
+                  </span>
+                  <div className="pl-1.5 md:pl-4">
+                    In the app and by email:
+                    <div className="mt-2 flex items-start gap-4 rounded-md bg-[#FFFFFF57] px-4 py-2 text-sm text-[#5D5757]">
+                      <p>💡</p>
+                      <div>
+                        “Ei, your people showed up big time! ❤️”
+                        <br />
+                        Duration: Nov 1, 2025 - Nov 6, 2025 <br /> Raised: GHS
+                        5,000
+                        <br />
+                        Our small fee: GHS 220
+                        <br />
+                        You receive: GHS 4,780
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-1.5">
+                    We know waiting is hard, so we move as fast as we possibly
+                    can.
+                    <br /> When your campaign ends, your money is basically
+                    already on the way.
+                    <p className="mt-1.5">
+                      Your money is safe, it’s truly yours.
+                      <br /> We’re in this together
+                      <span>✍</span>
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </article>
+
+          <article className="space-y-4" id="your-dashboard">
+            <h1 className="font-mackinac font-medium text-[#0E021A]">
+              7. Your Dashboard: Stay Close to Your Progress
+            </h1>
+            <div className="space-y-1.5 text-sm text-[#5D5757]">
+              <p>After launching, tap “My Dashboard” to see:</p>
+              <table>table</table>
+              <div>
+                <h5 className="font-bold">Donations Table</h5>
+                <table>table</table>
+
+                <p className="mt-1.5">
+                  <span className="font-bold">Actions:</span> Export list, send
+                  thank-you, share update
+                </p>
+              </div>
+            </div>
+          </article>
+
+          <article className="space-y-4" id="trust-and-safety">
+            <h1 className="font-mackinac font-medium text-[#0E021A]">
+              8. Trust & Safety (We’ve Got You)
+            </h1>
+            <div className="space-y-1.5 text-sm text-[#5D5757]">
+              <table>table</table>
+            </div>
+          </article>
+
+          <article className="space-y-4" id="need-help">
+            <h1 className="font-mackinac font-medium text-[#0E021A]">
+              9. Need Help? We’re Here (Like Family)
+            </h1>
+            <div className="space-y-2 text-sm text-[#5D5757]">
+              <ul className="list-inside list-disc">
+                <li>
+                  Reach us via our socials (we are{" "}
+                  <span className="font-bold">contreebute</span> everywhere)
+                </li>
+                <li>
+                  <span className="font-bold">Email</span>:{" "}
+                  <a href="mailto:hello@contreebute.io">hello@contreebute.io</a>
+                </li>
+              </ul>
+              <p className="italic">
+                No robots. Just real humans who want you to succeed.
+              </p>
+            </div>
+          </article>
+
+          <article
+            className="space-y-4 border-b border-[#c1c1c1] pb-3"
+            id="faq"
+          >
+            <h1 className="font-mackinac font-medium text-[#0E021A]">
+              10. Frequently Asked Questions (FAQ)
+            </h1>
+            <div className="space-y-2 text-sm text-[#5D5757]">
+              <div className="space-y-1">
+                <h5 className="font-bold">Q: Do I pay to start a campaign?</h5>
+                <p>
+                  <span className="font-bold">A:</span> Never. It’s 100% free to
+                  create and launch.
+                </p>
+              </div>
+              <div className="space-y-1">
+                <h5 className="font-bold">Q: When do I pay fees?</h5>
+                <p>
+                  <span className="font-bold">A:</span> Only after your campaign
+                  ends — and only if you raise money.
+                </p>
+              </div>
+              <div className="space-y-1">
+                <h5 className="font-bold">
+                  Q: Can I change things after launch?
+                </h5>
+                <p>
+                  <span className="font-bold">A:</span> Yes! Edit name, items,
+                  dates, or payout method anytime.
+                </p>
+              </div>
+              <div className="space-y-1">
+                <h5 className="font-bold">Q: What if I don’t hit my goal?</h5>
+                <p>
+                  <span className="font-bold">A:</span> You still keep every
+                  cedi raised (minus tiny fees).
+                </p>
+              </div>
+              <div className="space-y-1">
+                <h5 className="font-bold">Q: How do I withdraw?</h5>
+                <p>
+                  <span className="font-bold">A:</span> Request a payout or
+                  automatically receive your funds 1–3 days in your MoMo or
+                  bank.
+                </p>
+              </div>
+              <div className="space-y-1">
+                <h5 className="font-bold">Q: Is my data safe?</h5>
+                <p>
+                  <span className="font-bold">A:</span> Yes. We use bank-level
+                  encryption. Your trust is everything.
+                </p>
+              </div>
+            </div>
+          </article>
+
+          <article className="space-y-2 text-sm text-[#5D5757]">
+            <div className="space-y-1.5 border-b border-[#c1c1c1] pb-3">
+              <h5 className="font-bold">You’re Ready to Begin</h5>
+              <p>
+                Whether you’re{" "}
+                <span className="font-bold">asking with hope</span> or{" "}
+                <span className="font-bold">giving with love</span>,{" "}
+                <span className="font-bold">Contreebute</span> is your safe
+                space. <br />
+                <span className="font-bold">
+                  No pressure. No judgment. Just kindness in action.
+                </span>
+              </p>
+            </div>
+            <div className="space-y-1.5 border-b border-[#c1c1c1] pb-3">
+              <div>
+                <h5 className="font-bold">Start Your Campaign</h5>
+                <p>Create Now – It’s free, fast and full of love.</p>
+              </div>
+              <div>
+                <h5 className="font-bold">Or Explore Campaigns</h5>
+                <p>Browse Dreams – Be someone’s answer today.</p>
+              </div>
+            </div>
+            <div className="border-l-2 border-[#404040] pl-2">
+              “One campaign can change a life.
+              <br /> One donation can change the world.
+              <br /> Thank you for being part of this family.”
+            </div>
+            <div className="mt-5">
+              With impact, <br />{" "}
+              <span className="font-bold">The Contreebute Team</span>
+            </div>
           </article>
         </section>
 
