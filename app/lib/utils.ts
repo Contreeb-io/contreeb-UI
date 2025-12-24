040b6b5862c668ad27861e7987846eff6a2b3fbb
saeedyussif663 <saeedyussif663@gmail.com>
2025-12-24 08:48:00 +0000

    Create a function for dashboard loader

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { redirect } from "react-router";
+import { twMerge } from "tailwind-merge";
+import { TOKEN_KEY } from "../context/auth-context";
+import http, { getAuthToken } from "./http";
+import token from "./token";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function dashboardLoader() {
+  const userToken = getAuthToken?.() || token.get(TOKEN_KEY);
+  if (!userToken) {
+    throw redirect("/");
+  }
+  const res = await http.get("campaigns");
+  return res;
 }
