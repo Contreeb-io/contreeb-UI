0b96823aed5b72bf79355dac817164b67bad9bbf
saeedyussif663 <saeedyussif663@gmail.com>
2025-12-24 08:44:38 +0000

    Make the select dynamic to the campaigns created

--- app/components/dashboard/layout.tsx
+++ app/components/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import { PlusIcon } from "lucide-react";
 import { useEffect } from "react";
-import { Outlet, useLocation } from "react-router";
+import { Link, Outlet, useLoaderData, useLocation } from "react-router";
 import {
   Select,
   SelectContent,
@@ -15,6 +15,7 @@ import { AppSidebar } from "./app-sidebar";
 function LayoutContent() {
   const { open, openMobile, isMobile } = useSidebar();
   const isSidebarOpen = isMobile ? openMobile : open;
+  const data = useLoaderData();
 
   return (
     <main className="sidebar flex-1 space-y-8 px-4 pt-6 pb-10 md:space-y-14 md:px-6">
@@ -26,22 +27,23 @@ function LayoutContent() {
           </SelectTrigger>
           <SelectContent className="sidebar p-3 font-medium text-[#0E021A]">
             <SelectGroup>
-              {/* <SelectLabel>Fruits</SelectLabel> */}
-              <SelectItem
-                value="apple"
-                className="rounded-none border-b border-[#F0F2F5]"
+              {/* fix type later */}
+              {data?.map((item: any) => (
+                <SelectItem
+                  id={item.id}
+                  value={item.id}
+                  className="rounded-none border-b border-[#F0F2F5]"
+                >
+                  {item.value}
+                </SelectItem>
+              ))}
+
+              <Link
+                to={"/create-campaign"}
+                className="mt-2 flex items-center justify-center rounded-full border-b border-[#F0F2F5] bg-[#F1F1FF] p-2 text-sm font-semibold text-[#6360F0]"
               >
-                Apple
-              </SelectItem>
-              <SelectItem
-                value="banana"
-                className="rounded-none border-b border-[#F0F2F5]"
-              >
-                Banana
-              </SelectItem>
-              <div className="mt-2 flex items-center justify-center rounded-full border-b border-[#F0F2F5] bg-[#F1F1FF] p-2 text-sm font-semibold text-[#6360F0]">
                 <PlusIcon color="#6360F0" /> Create a campaign
-              </div>
+              </Link>
             </SelectGroup>
           </SelectContent>
         </Select>
